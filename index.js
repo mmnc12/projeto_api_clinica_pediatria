787f9aa97248f1a46ca984da800836586b854f1d
Manoel Mecias <mmnc12@gmail.com>
2025-05-27 22:52:30 -0300

    configurando

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
 import { retornaMedicos } from './servicos/retorona_medicos.js';
 
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
 
 app.get('/medicos', async (req, res) => {
-  const medicos = await retornaMedicos();
-  res.json(medicos);
+  try {
+    const medicos = await retornaMedicos();
+    res.json(medicos);
+  } catch (error) {
+    console.error("Erro ao buscar o médico:", error);
+    res.status(500).json({ mensagem: "Erro ao buscar os dados." })
+  }
 })
 
 app.listen(8080, () => {
--- servicos/conexao.js
+++ servicos/conexao.js
@@ -1,10 +1,25 @@
 import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+
+dotenv.config(); // Carrega as variáveis de ambiente
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'clinicapediatradb'
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
 });
 
 export default pool;
+
+
+// import mysql from 'mysql2/promise';
+
+// const pool = mysql.createPool({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '',
+//   database: 'clinicapediatradb'
+// });
+
+// export default pool;
